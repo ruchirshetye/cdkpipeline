6b20b75b3ed05e7ee091ce44889979e8d25d8432
ruchirshetye <ruchirshetye@gmail.com>
2020-10-18 20:59:07 +0530

    first commit

---
+++ bin/cdkpipelines-demo.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import { App } from '@aws-cdk/core';
+import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-pipeline-stack';
+
+const app = new App();
+
+new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {
+  env: { account: '997785413584', region: 'us-east-1' },
+});
+
+app.synth();
